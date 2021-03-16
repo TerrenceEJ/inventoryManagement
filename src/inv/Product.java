7661dcc5a4346a759082f903eb0a1e2d659bd85b
TerrenceEJ <terrencejones0820@gmail.com>
2021-03-15 20:57:16 -0400

    basic outline to start working on inventory

---
+++ src/inv/Product.java
@@ -0,0 +1,11 @@
+package inv;
+
+public class Product {
+
+
+	public char[] getName() {
+		// TODO Auto-generated method stub
+		return null;
+	} //placeholder for product class (add items from inventory here along with their outputted barcode number)
+
+}
